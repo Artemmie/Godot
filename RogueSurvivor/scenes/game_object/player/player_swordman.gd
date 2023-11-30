ae54494716ee0e7ee23e5ce43c23dc050f70daf6
Artooof <artemgerasimov55@gmail.com>
2023-11-30 15:31:26 +0200

    Generic death animation for enemy Movement animation for player and enemy Axe ability added to the dictionary Added limited amount of upgrades available to choose from the pool

--- RogueSurvivor/scenes/game_object/player/player_swordman.gd
+++ RogueSurvivor/scenes/game_object/player/player_swordman.gd
@@ -6,6 +6,9 @@ const ACCELERATION_SMOOTHING = 10
 @onready var damage_interval_timer = $DamageIntervalTimer
 @onready var health_component = $HealthComponent
 @onready var health_bar = $HealthBar
+@onready var abilities = $Abilities
+@onready var animation_player = $AnimationPlayer
+@onready var visuals = $Visuals
 
 var number_colliding_bodies = 0
 
@@ -14,14 +17,24 @@ func _ready():
 	$CollisionArea2D.body_exited.connect(on_body_exited)
 	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
 	health_component.health_changed.connect(on_health_changed)
+	GameEvents.ability_upgrades_added.connect(on_ability_upgrade_added)
 	update_health_display()
 
 func _process(delta):
 	var movement_vector = get_movement_vector()
 	var direction = movement_vector.normalized()
-	var trarget_velocity = direction * MAX_SPEED
-	velocity  = velocity.lerp(trarget_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
+	var target_velocity = direction * MAX_SPEED
+	velocity  = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
 	move_and_slide()
+	if movement_vector.x != 0 || movement_vector.y != 0:
+		animation_player.play("walk")
+	else:
+		animation_player.play("RESET")
+	var move_sign = sign(movement_vector.x)
+	if move_sign != 0:
+		visuals.scale= Vector2(move_sign, 1)
+	
+	
 func  get_movement_vector():
 	return Vector2(Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), Input.get_action_strength("move_down") - Input.get_action_strength("move_up"))
 
@@ -46,3 +59,10 @@ func on_damage_interval_timer_timeout():
 	
 func on_health_changed():
 	update_health_display()
+	
+func on_ability_upgrade_added(ability_upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	if not ability_upgrade is Ability:
+		return
+	var ability = ability_upgrade as Ability
+	abilities.add_child(ability.ability_controller_scene.instantiate())
+	
